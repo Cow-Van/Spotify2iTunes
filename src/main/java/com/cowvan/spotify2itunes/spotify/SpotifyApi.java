3c384b2daa81c6f1e827ab01d2c47569d2a12ce5
Calvin <calvinlu@live.com>
2022-12-15 13:23:44 -0800

    Add getting playlist songs to SpotifyApi

--- src/main/java/com/cowvan/spotify2itunes/spotify/SpotifyApi.java
+++ src/main/java/com/cowvan/spotify2itunes/spotify/SpotifyApi.java
@@ -2,16 +2,14 @@ package com.cowvan.spotify2itunes.spotify;
 
 import com.cowvan.spotify2itunes.utils.ParseUtils;
 import com.cowvan.spotify2itunes.utils.RequestUtils;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
-import java.util.Base64;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class SpotifyApi {
     private final String clientId;
@@ -37,6 +35,33 @@ public class SpotifyApi {
         JSONObject body = ParseUtils.parseJSONStringToJSONObject(response.body());
 
         return body.getJSONObject("tracks");
+    }
+
+    public Song[] getPlaylistSongs(String playlistId) throws URISyntaxException, IOException, InterruptedException {
+        ArrayList<Song> songs = new ArrayList<>();
+
+        JSONArray tracks = this.getPlaylist(playlistId).getJSONArray("items");
+
+        for (Object object : tracks) {
+            JSONObject track = (JSONObject) object;
+            JSONObject trackInfo = track.getJSONObject("track");
+
+            String trackName = trackInfo.getString("name");
+            ArrayList<String> trackArtists = new ArrayList<>();
+
+            JSONArray trackArtistsData = trackInfo.getJSONArray("artists");
+
+            for (Object trackArtistData : trackArtistsData) {
+                JSONObject trackArtist = (JSONObject) trackArtistData;
+                trackArtists.add(trackArtist.getString("name"));
+            }
+
+            String trackImage = trackInfo.getJSONObject("album").getJSONArray("images").getJSONObject(0).getString("url");
+
+            songs.add(new Song(trackName, trackArtists.toArray(new String[0]), trackImage));
+        }
+
+        return songs.toArray(new Song[0]);
     }
 
     private void updateAccessToken() throws URISyntaxException, IOException, InterruptedException {
