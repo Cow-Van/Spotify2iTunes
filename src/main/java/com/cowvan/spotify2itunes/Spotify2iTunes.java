5073d4ffd0f8b8cf2cf1eb89bf91ba0d02d6f47a
Calvin <calvinlu@live.com>
2022-12-14 21:41:03 -0800

    Add SpotifyApi to access Spotify API

--- src/main/java/com/cowvan/spotify2itunes/Spotify2iTunes.java
+++ src/main/java/com/cowvan/spotify2itunes/Spotify2iTunes.java
@@ -1,10 +1,40 @@
 package com.cowvan.spotify2itunes;
 
-import info.schnatterer.itunes4j.ITunesException;
+import com.cowvan.spotify2itunes.spotify.SpotifyApi;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Spotify2iTunes {
-    public static void main(String[] args) throws ITunesException {
+    public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
 //        BetteriTunes iTunes = new BetteriTunes();
-//        BetterPlaylist p = iTunes.createPlaylist("Foo");
+        SpotifyApi spotifyApi = new SpotifyApi(System.getenv("SPOTIFY_CLIENT_ID"), System.getenv("SPOTIFY_CLIENT_SECRET"));
+
+        JSONArray tracks = spotifyApi.getPlaylist("1OeL09zxHjIxafVJjFrB55").getJSONArray("items");
+
+        for (Object object : tracks) {
+            JSONObject track = (JSONObject) object;
+            JSONObject trackInfo = track.getJSONObject("track");
+
+            String trackName = trackInfo.getString("name");
+            ArrayList<String> trackArtists = new ArrayList<>();
+
+            JSONArray trackArtistsData = trackInfo.getJSONArray("artists");
+
+            for (Object trackArtistData : trackArtistsData) {
+                JSONObject trackArtist = (JSONObject) trackArtistData;
+                trackArtists.add(trackArtist.getString("name"));
+            }
+
+            String trackImage = trackInfo.getJSONObject("album").getJSONArray("images").getJSONObject(0).getString("url");
+
+            System.out.println(trackName);
+            System.out.println(Arrays.toString(trackArtists.toArray()));
+            System.out.println(trackImage);
+        }
     }
 }
