db39f8dce1cf5c3c3e57949322c3ba68cd3282bb
Cow-Van <90744327+Cow-Van@users.noreply.github.com>
2023-01-20 22:10:14 -0800

    Add downloadSong method for YouTubeApi

--- src/main/java/com/cowvan/spotify2itunes/youtube/YouTubeApi.java
+++ src/main/java/com/cowvan/spotify2itunes/youtube/YouTubeApi.java
@@ -1,26 +1,33 @@
 package com.cowvan.spotify2itunes.youtube;
 
+import com.cowvan.spotify2itunes.Constants;
+import com.cowvan.spotify2itunes.Spotify2iTunes;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class YouTubeApi {
     public YouTubeApi() {
     }
 
-    public static void downloadSong(String name) throws IOException, InterruptedException {
-//        Process ytdlpProcess = new ProcessBuilder(Constants.ytdlpWorstVideoBestVideo + " " + name)
-//                .redirectErrorStream(true)
-//                .start();
-//        BufferedReader ytdlpInputStreamReader = new BufferedReader(new InputStreamReader(ytdlpProcess.getInputStream()));
-//        String ytdlpProcessInputStreamLine;
-//
-//        Spotify2iTunes.CONSOLE.printf(String.valueOf(ytdlpInputStreamReader.readLine() != null));
-//
-//        while ((ytdlpProcessInputStreamLine = ytdlpInputStreamReader.readLine()) != null) {
-//            Spotify2iTunes.CONSOLE.printf(ytdlpProcessInputStreamLine + "\n ");
-//        }
-//
-//        ytdlpProcess.waitFor();
-//
-//        ytdlpInputStreamReader.close();
+    public boolean downloadSong(String url) throws IOException, InterruptedException {
+        Process ytdlpProcess = new ProcessBuilder(Constants.ytdlpWorstVideoBestVideo + " \"" + url + "\"")
+                .redirectErrorStream(true)
+                .start();
+        BufferedReader ytdlpInputStreamReader = new BufferedReader(new InputStreamReader(ytdlpProcess.getInputStream()));
+        String ytdlpProcessInputStreamLine;
+
+        Spotify2iTunes.CONSOLE.printf(String.valueOf(ytdlpInputStreamReader.readLine() != null));
+
+        while ((ytdlpProcessInputStreamLine = ytdlpInputStreamReader.readLine()) != null) {
+            Spotify2iTunes.CONSOLE.printf(ytdlpProcessInputStreamLine + "\n ");
+        }
+
+        ytdlpProcess.waitFor();
+
+        ytdlpInputStreamReader.close();
+
+        return ytdlpProcess.exitValue() == 0;
     }
 }
