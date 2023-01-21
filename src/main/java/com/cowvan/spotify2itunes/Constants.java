b3726eb29a893911eecdd8207a333be086a3819d
Cow-Van <90744327+Cow-Van@users.noreply.github.com>
2023-01-20 22:08:17 -0800

    Fix worst audio best video constant naming Add yt-dlp test constant

--- src/main/java/com/cowvan/spotify2itunes/Constants.java
+++ src/main/java/com/cowvan/spotify2itunes/Constants.java
@@ -1,13 +1,28 @@
 package com.cowvan.spotify2itunes;
 
+import com.cowvan.spotify2itunes.command.Command;
+import com.cowvan.spotify2itunes.command.argument.Flag;
+import com.cowvan.spotify2itunes.command.argument.Option;
+
 public final class Constants {
     public static final String wingetYtdlpPackageName = "yt-dlp";
     public static final String ytdlpCommand = "yt-dlp";
     public static final String wingetCommand = "winget";
-//    public static String ytdlpWorstVideoBestVideo =
-//            new StringBuilder()
-//                    .append(ytdlpCommand)
-//                    .append("--format")
-//                    .toString();
-//            ('%s --format \"wv+ba\" --extract-audio --audio-format \"wav\" --audio-quality \"0\" -j --no-simulate --progress-template {\"progressPercentage\":\"%(progress._percent_str)s\",\"progressTotal\":\"%(progress._total_bytes_str)s\",\"speed\":\"%(progress._speed_str)s\",\"ETA\":\"%(progress._eta_str)s\"}').formatted(ytdlpCommand);
+    public static String ytdlpWorstAudioBestVideo =
+            new Command.CommandBuilder(ytdlpCommand)
+                    .addOption(Option.literal("--format", "\"wv+ba\""))
+                    .addFlag(Flag.literal("--extract-audio"))
+                    .addOption(Option.literal("--audio-format", "\"wav\""))
+                    .addOption(Option.literal("--audio-quality", "\"0\""))
+                    .addFlag(Flag.literal("--dump-single-json"))
+                    .addFlag(Flag.literal("--no-simulate"))
+                    .addOption(Option.literal("--progress-template", """
+                                                                     {"progressPercentage": "%(progress._percent_str)s", "progressTotal": "%(progress._total_bytes_str)s", "speed":"%(progress._speed_str)s", "ETA": "%(progress._eta_str)s"}
+                                                                     """))
+                    .build()
+                    .asString();
+    public static String ytdlpTest = new Command.CommandBuilder("yt-dlp")
+            .addFlag(Flag.literal("--version"))
+            .build()
+            .asString();
 }
